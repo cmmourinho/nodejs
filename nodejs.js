6971214e82924283b45d83ac1cf4204fe9c25318
Tanaboon Chaichaur <tanaboon.cha@ttbbank.com>
2024-01-30 21:46:41 +0700

    edit

--- nodejs.js
+++ nodejs.js
@@ -1,7 +1,7 @@
 // Import the built-in 'http' module
 const http = require('http');
 
-// Configure the HTTP server to respond with "Hello, World!" to all requests
+// onfigure the HTTP server to respond with "Hello, World!" to all requests
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, World!\n');
