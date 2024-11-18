3697ce119a7696d3e66ade7adc557c4919376412
Tanaboon Chaichaur <tanaboon.cha@ttbbank.com>
2024-11-18 14:01:38 +0700

    add

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-/// Import the built-in 'http' module
+// Import the built-in 'http' module
 const http = require('http');
 
-// onfigure the HTTP server to respond with "Hello, World!" to all requests
+// Configure the HTTP server to respond with "Hello, World!" to all requests
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, World!\n');
 });
 
-///// Listen on port 3000 and IP address 127.0.0.1
+// Listen on port 3000 and IP address 127.0.0.1
 const PORT = 3000;
 const IP = '127.0.0.1';
 
